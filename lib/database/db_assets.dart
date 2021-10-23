ebe3ffce1dc83bb4443888e925f36b3c70cc0949
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-10-23 10:18:42 +0200

    Update database - added new assets.

--- lib/database/db_assets.dart
+++ lib/database/db_assets.dart
@@ -60,6 +60,105 @@ class DatabaseAssets {
         explosionClass: ExplosionClass(
             compatibilityGroup:
                 DatabaseCompatibilityGroup.container[CompatibilityGroupType.E]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.2]!)),
+    BattleAirAssetType.BBU35: new BattleAirAsset(
+        type: BattleAirAssetType.BBU35,
+        boxType: BoxType.BBU35BOX,
+        name: "BBU-35/B",
+        weights: Weights(gross: 10, netExplosive: 4, net: 10),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.S]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.4]!)),
+    BattleAirAssetType.BBU36: new BattleAirAsset(
+        type: BattleAirAssetType.BBU36,
+        boxType: BoxType.BBU36BOX,
+        name: "BBU-36/B",
+        weights: Weights(gross: 17, netExplosive: 10, net: 17),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.S]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.4]!))
+    ,
+    //TODO: Sprawdzić wagę cxu
+    BattleAirAssetType.CXU3: new BattleAirAsset(
+        type: BattleAirAssetType.CXU3,
+        boxType: BoxType.M2A1,
+        name: "CXU-3A/B",
+        weights: Weights(gross: 4, netExplosive: 3, net: 4),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.G]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.4]!))
+    ,
+    BattleAirAssetType.MJU7AB: new BattleAirAsset(
+        type: BattleAirAssetType.MJU7AB,
+        boxType: BoxType.MJU7ABOX,
+        name: "MJU-7A/B",
+        weights: Weights(gross: 370, netExplosive: 261, net: 370),
+        dimensions: Dimensions(height: 205, length: 52, width: 25),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.G]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.3]!))
+    ,
+    BattleAirAssetType.MK82: new BattleAirAsset(
+        type: BattleAirAssetType.MK82,
+        boxType: BoxType.MHU149,
+        name: "MK-82",
+        weights: Weights(gross: 227000, netExplosive: 87000, net: 227000),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.D]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.1]!))
+    ,
+    BattleAirAssetType.MK84: new BattleAirAsset(
+        type: BattleAirAssetType.MK84,
+        boxType: BoxType.MK84BOX,
+        name: "MK-84",
+        weights: Weights(gross: 925000, netExplosive: 428000, net: 925000),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.D]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.1]!))
+    ,
+    BattleAirAssetType.MPLD: new BattleAirAsset(
+        type: BattleAirAssetType.MPLD,
+        boxType: BoxType.M548MPLD,
+        name: "20 mm MP LD M70 A1",
+        weights: Weights(gross: 270, netExplosive: 48, net: 270),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.G]!,
             explosionSubclass: DatabaseExplosionSubclasses.container[1.2]!))
+    ,
+    BattleAirAssetType.TPRRR: new BattleAirAsset(
+        type: BattleAirAssetType.TPRRR,
+        boxType: BoxType.M548_TPRRR,
+        name: "20 mm TP RRR LD M2",
+        weights: Weights(gross: 270, netExplosive: 39, net: 270),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.C]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.4]!))
+    ,
+    BattleAirAssetType.RR170AL: new BattleAirAsset(
+        type: BattleAirAssetType.RR170AL,
+        boxType: BoxType.CHAFF_BOX,
+        name: "RR-170A/AL",
+        weights: Weights(gross: 140, netExplosive: 0, net: 140),
+        dimensions: Dimensions(height: 1, length: 1, width: 1),
+        explosionClass: ExplosionClass(
+            compatibilityGroup:
+            DatabaseCompatibilityGroup.container[CompatibilityGroupType.S]!,
+            explosionSubclass: DatabaseExplosionSubclasses.container[1.4]!))
+    ,
   };
 }
--- lib/database/db_boxes.dart
+++ lib/database/db_boxes.dart
@@ -101,5 +101,177 @@ class DatabaseBoxes {
         battleAirAsset: DatabaseAssets.container[BattleAirAssetType.AIM9x]!,
         type: BoxType.CNU609
     ),
+    BoxType.BBU35BOX: new Box(
+        name: "BBU35BOX",
+        capacities: Capacities(
+            maximum: 4
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.BBU35]!,
+        type: BoxType.BBU35BOX
+    ),
+    BoxType.BBU36BOX: new Box(
+        name: "BBU36BOX",
+        capacities: Capacities(
+            maximum: 4
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.BBU36]!,
+        type: BoxType.BBU36BOX
+    ),
+    BoxType.M2A1: new Box(
+        name: "M2A1",
+        capacities: Capacities(
+            maximum: 50
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.CXU3]!,
+        type: BoxType.M2A1
+    ),
+    BoxType.MJU7ABOX: new Box(
+        name: "MJU7ABOX",
+        capacities: Capacities(
+            maximum: 50
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.MJU7AB]!,
+        type: BoxType.MJU7ABOX
+    ),
+    BoxType.MK84BOX: new Box(
+        name: "MK84BOX",
+        capacities: Capacities(
+            maximum: 1
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.MK84]!,
+        type: BoxType.MK84BOX
+    ),
+    BoxType.MHU149: new Box(
+        name: "MHU149",
+        capacities: Capacities(
+            maximum: 1
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.MK82]!,
+        type: BoxType.MHU149
+    ),
+    BoxType.M548MPLD: new Box(
+        name: "M548MPLD",
+        capacities: Capacities(
+            maximum: 200
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.MPLD]!,
+        type: BoxType.M548MPLD
+    ),
+    BoxType.M548_TPRRR: new Box(
+        name: "M548_TPRRR",
+        capacities: Capacities(
+            maximum: 200
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.TPRRR]!,
+        type: BoxType.M548_TPRRR
+    ),
+    BoxType.CHAFF_BOX: new Box(
+        name: "CHAFF_BOX",
+        capacities: Capacities(
+            maximum: 50
+        ),
+        weights: BoxWeights(
+            gross: 1,
+            net: 1,
+            netExplosive: 1
+        ),
+        dimensions: Dimensions(
+            height: 1,
+            width: 1,
+            length: 1
+        ),
+        maxStackLevel: 2,
+        battleAirAsset: DatabaseAssets.container[BattleAirAssetType.RR170AL]!,
+        type: BoxType.CHAFF_BOX
+    ),
+
   };
 }
