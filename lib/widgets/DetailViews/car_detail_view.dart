02c3e9bcd2292a835670e9b46d50dd9abdb7d2b0
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-02 18:13:26 +0100

    Update car detail view - code review.

--- lib/widgets/DetailViews/car_detail_view.dart
+++ lib/widgets/DetailViews/car_detail_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tobaa/widgets/Constants/app_colors.dart';
-import 'package:tobaa/battle_air_asset/battle_air_asset.dart';
 import 'package:tobaa/car/car.dart';
-import 'package:tobaa/database/db_assets.dart';
-import 'package:tobaa/enumerators/baa_type.dart';
-import 'package:tobaa/stack/stack.dart' as ContainerStack;
-import 'package:tobaa/widgets/Constants/url.dart';
+import 'package:tobaa/widgets/ListViews/stack_list_view.dart';
+
 import 'package:tobaa/widgets/SmallWidgets/property_view.dart';
 
 import '../../main.dart';
@@ -66,67 +63,12 @@ class CarDetailView extends StatelessWidget {
                   ],
                 ),
               ),
-              this._listViewStack(this._car.stacks)
+              stackListView(this._car.stacks)
             ],
           ))),
     );
   }
 
-  Widget _listViewStack(List<ContainerStack.Stack> stacks) {
-    return new ListView.builder(
-      scrollDirection: Axis.vertical,
-      shrinkWrap: true,
-      itemCount: stacks.length,
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-              onTap: () {
-                TOBAAApp.selectedStack = stacks.elementAt(index);
-                Navigator.pushNamed(context, Url.STACK_DETAIL);
-              },
-              title: this._stack(stacks.elementAt(index), index)),
-        );
-      },
-    );
-  }
 
-  Widget _stack(ContainerStack.Stack stack, int index) {
-    String baa = "";
-    Map<BattleAirAssetType, int> container = {};
 
-    stack.levels.forEach((level) {
-      level.boxes.forEach((box) {
-        var value = 0;
-        if (container.containsKey(box.battleAirAsset.type))
-          value = container[box.battleAirAsset.type]!;
-        container[box.battleAirAsset.type] = value + box.capacities.current;
-      });
-    });
-
-    container.forEach((key, value) {
-      var ba = DatabaseAssets.container[key]!;
-      baa += "${ba.name}: $value szt., ";
-    });
-
-    return Container(
-      color: Color(AppColors.STACK),
-      child: Column(
-        children: [
-          Text('${Strings.STACK} ${index + 1}: ',
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          Text('${Strings.NEW} ${massConverter(stack.weights.netExplosive)}'),
-          Text('${Strings.NET_WEIGHT} ${massConverter(stack.weights.net)}'),
-          Text('${Strings.GROSS_WEIGHT} ${massConverter(stack.weights.gross)}'),
-          Text(
-              '${Strings.NUMBER_OF_THE_CONTAINERS} ${stack.currentNumberOfBoxes}'),
-          Text(
-              '${Strings.NUMBER_OF_THE_BAA} ${stack.battleAirAssetCapacities.current}'),
-          Text('$baa')
-        ],
-      ),
-    );
-  }
 }
---
+++ lib/widgets/ListViews/stack_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:tobaa/widgets/Constants/url.dart';
+import 'package:tobaa/widgets/ListItems/stack_list_item.dart';
+import 'package:tobaa/stack/stack.dart' as ContainerStack;
+import '../../main.dart';
+
+Widget stackListView(List<ContainerStack.Stack> stacks) {
+  return new ListView.builder(
+    scrollDirection: Axis.vertical,
+    shrinkWrap: true,
+    itemCount: stacks.length,
+    itemBuilder: (context, index) {
+      return Card(
+        child: ListTile(
+            onTap: () {
+              TOBAAApp.selectedStack = stacks.elementAt(index);
+              Navigator.pushNamed(context, Url.STACK_DETAIL);
+            },
+            title: stackItemView(stacks.elementAt(index), index)),
+      );
+    },
+  );
+}
