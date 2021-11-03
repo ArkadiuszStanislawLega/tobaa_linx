f7917cbc320c87392b21184136cc855c80d5ad1b
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-03 19:15:15 +0100

    Update stack list item  - code review.

--- lib/widgets/ListItems/stack_list_item.dart
+++ lib/widgets/ListItems/stack_list_item.dart
@@ -1,48 +1,59 @@
 import 'package:flutter/widgets.dart';
 import 'package:tobaa/database/db_assets.dart';
 import 'package:tobaa/enumerators/baa_type.dart';
+import 'package:tobaa/stack/stack.dart' as ContainerStack;
 import 'package:tobaa/widgets/Constants/app_colors.dart';
 import 'package:tobaa/widgets/Constants/strings.dart';
-import 'package:tobaa/stack/stack.dart' as ContainerStack;
 
 import '../../main.dart';
 
-Widget stackItemView(ContainerStack.Stack stack, int index) {
-  String baa = "";
-  Map<BattleAirAssetType, int> container = {};
+class StackItemView extends StatelessWidget {
+  final ContainerStack.Stack _stack;
+  final int _index;
 
-  stack.levels.forEach((level) {
-    level.boxes.forEach((box) {
-      var value = 0;
-      if (container.containsKey(box.battleAirAsset.type))
-        value = container[box.battleAirAsset.type]!;
-      container[box.battleAirAsset.type] = value + box.capacities.current;
+  StackItemView(this._stack, this._index);
+
+  @override
+  Widget build(BuildContext context) {
+    String baa = "";
+    Map<BattleAirAssetType, int> container = {};
+    this._stack.levels.forEach((level) {
+      level.boxes.forEach((box) {
+        var value = 0;
+        if (container.containsKey(box.battleAirAsset.type))
+          value = container[box.battleAirAsset.type]!;
+        container[box.battleAirAsset.type] = value + box.capacities.current;
+      });
     });
-  });
 
-  container.forEach((key, value) {
-    var ba = DatabaseAssets.container[key]!;
-    baa += "${ba.name}: $value szt., ";
-  });
+    container.forEach((key, value) {
+      var ba = DatabaseAssets.container[key]!;
+      baa += "${ba.name}: $value szt., ";
+    });
 
-  return Container(
-    color: Color(AppColors.STACK),
-    child: Column(
-      children: [
-        Text('${Strings.STACK} ${index + 1}: ',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
+    return Container(
+      color: Color(AppColors.STACK),
+      child: Column(
+        children: [
+          Text(
+            '${Strings.STACK} ${this._index + 1}: ',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        Text('${Strings.NEW} ${massConverter(stack.weights.netExplosive)}'),
-        Text('${Strings.NET_WEIGHT} ${massConverter(stack.weights.net)}'),
-        Text('${Strings.GROSS_WEIGHT} ${massConverter(stack.weights.gross)}'),
-        Text(
-            '${Strings.NUMBER_OF_THE_CONTAINERS} ${stack.currentNumberOfBoxes}'),
-        Text(
-            '${Strings.NUMBER_OF_THE_BAA} ${stack.battleAirAssetCapacities.current}'),
-        Text('$baa')
-      ],
-    ),
-  );
+          Text(
+              '${Strings.NEW} ${massConverter(this._stack.weights.netExplosive)}'),
+          Text(
+              '${Strings.NET_WEIGHT} ${massConverter(this._stack.weights.net)}'),
+          Text(
+              '${Strings.GROSS_WEIGHT} ${massConverter(this._stack.weights.gross)}'),
+          Text(
+              '${Strings.NUMBER_OF_THE_CONTAINERS} ${this._stack.currentNumberOfBoxes}'),
+          Text(
+              '${Strings.NUMBER_OF_THE_BAA} ${this._stack.battleAirAssetCapacities.current}'),
+          Text('$baa')
+        ],
+      ),
+    );
+  }
 }
