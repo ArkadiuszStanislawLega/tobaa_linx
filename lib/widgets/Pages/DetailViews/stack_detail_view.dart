305b220299326718e24ebdcc875b505c93f4bd83
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-09 15:01:44 +0100

    Update stack detail view - properties style.

--- lib/widgets/Pages/DetailViews/stack_detail_view.dart
+++ lib/widgets/Pages/DetailViews/stack_detail_view.dart
@@ -4,7 +4,7 @@ import 'package:tobaa/Constants/app_colors.dart';
 import 'package:tobaa/Constants/strings.dart';
 import 'package:tobaa/stack/stack.dart' as ContainerStack;
 import 'package:tobaa/widgets/ListViews/stack_levels_list_view.dart';
-import 'package:tobaa/widgets/Templates/property_template.dart';
+import 'package:tobaa/widgets/Templates/property_game_template.dart';
 import 'package:tobaa/widgets/Templates/property_with_hint_template.dart';
 
 import '../../../main.dart';
@@ -34,48 +34,49 @@ class StackDetailView extends StatelessWidget {
           color: Color(AppColors.STACK),
           child: TabBarView(
             children: [
-              SingleChildScrollView(
-                child: Column(
-                  children: [
-                    PropertyTemplate(
-                      name: Strings.WIDTH,
-                      value: '${sizeConverter(this._stack.dimensions.width)}',
-                    ),
-                    PropertyTemplate(
-                      name: Strings.LENGTH,
-                      value: '${sizeConverter(this._stack.dimensions.length)}',
-                    ),
-                    PropertyTemplate(
-                      name: Strings.HEIGHT,
-                      value: '${sizeConverter(this._stack.dimensions.height)}',
-                    ),
-                    PropertyTemplate(
-                      name: Strings.NEW,
-                      value:
-                          '${massConverter(this._stack.weights.netExplosive)}',
-                    ),
-                    PropertyWithHintTemplate(
-                      hint: Strings.HINT_NET_WEIGHT,
-                      propertyName: Strings.NET_WEIGHT,
-                      propertyValue:
-                          '${massConverter(this._stack.weights.net)}',
-                    ),
-                    PropertyWithHintTemplate(
-                      hint: Strings.HINT_GROSS_WEIGHT,
-                      propertyName: Strings.GROSS_WEIGHT,
-                      propertyValue:
-                          '${massConverter(this._stack.weights.gross)}',
-                    ),
-                    PropertyTemplate(
-                      name: Strings.NUMBER_OF_THE_BAA,
-                      value: '${this._stack.battleAirAssetCapacities.current}',
-                    ),
-                    PropertyTemplate(
-                      name: Strings.MAX_STACK_LEVEL,
-                      value: '${this._stack.maximumStackLevel}',
-                    ),
-                  ],
-                ),
+              GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                children: [
+                  PropertyGameTemplate(
+                    name: Strings.WIDTH,
+                    value: '${sizeConverter(this._stack.dimensions.width)}',
+                  ),
+                  PropertyGameTemplate(
+                    name: Strings.LENGTH,
+                    value: '${sizeConverter(this._stack.dimensions.length)}',
+                  ),
+                  PropertyGameTemplate(
+                    name: Strings.HEIGHT,
+                    value: '${sizeConverter(this._stack.dimensions.height)}',
+                  ),
+                  PropertyGameTemplate(
+                    name: Strings.NEW,
+                    value: '${massConverter(this._stack.weights.netExplosive)}',
+                  ),
+                  PropertyWithHintTemplate(
+                    hint: Strings.HINT_NET_WEIGHT,
+                    propertyName: Strings.NET_WEIGHT,
+                    propertyValue: '${massConverter(this._stack.weights.net)}',
+                  ),
+                  PropertyWithHintTemplate(
+                    hint: Strings.HINT_GROSS_WEIGHT,
+                    propertyName: Strings.GROSS_WEIGHT,
+                    propertyValue:
+                        '${massConverter(this._stack.weights.gross)}',
+                  ),
+                  PropertyGameTemplate(
+                    name: Strings.NUMBER_OF_THE_BAA,
+                    value: '${this._stack.battleAirAssetCapacities.current}',
+                  ),
+                  PropertyGameTemplate(
+                    name: Strings.MAX_STACK_LEVEL,
+                    value: '${this._stack.maximumStackLevel}',
+                  ),
+                ],
               ),
               StackLevelsListView(this._stack.levels),
             ],
