62640f4d32959e5a4a9d12de4966ef16971cff3a
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-09 15:04:29 +0100

    Update box detail view - properties style.

--- lib/widgets/Pages/DetailViews/box_detail_view.dart
+++ lib/widgets/Pages/DetailViews/box_detail_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:tobaa/Constants/app_colors.dart';
 import 'package:tobaa/Constants/strings.dart';
 import 'package:tobaa/box/box.dart';
-import 'package:tobaa/widgets/Templates/property_template.dart';
+import 'package:tobaa/widgets/Templates/property_game_template.dart';
 import 'package:tobaa/widgets/Templates/property_with_hint_template.dart';
 
 import '../../../main.dart';
@@ -17,41 +16,40 @@ class BoxDetailView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('${this._box.name}')),
-      body: Container(
-        color: Color(AppColors.BOX),
-        child: new SingleChildScrollView(
-          child: Column(
-            children: [
-              PropertyTemplate(
-                name: Strings.WIDTH,
-                value: '${sizeConverter(this._box.dimensions.width)}',
-              ),
-              PropertyTemplate(
-                name: Strings.LENGTH,
-                value: '${sizeConverter(this._box.dimensions.length)}',
-              ),
-              PropertyTemplate(
-                name: Strings.HEIGHT,
-                value: '${sizeConverter(this._box.dimensions.height)}',
-              ),
-              PropertyWithHintTemplate(
-                hint: Strings.HINT_NET_WEIGHT,
-                propertyName: Strings.NET_WEIGHT,
-                propertyValue: '${massConverter(this._box.weights.net)}',
-              ),
-              PropertyWithHintTemplate(
-                hint: Strings.HINT_GROSS_WEIGHT,
-                propertyName:Strings.GROSS_WEIGHT,
-                propertyValue: '${massConverter(this._box.weights.currentGross)}',
-              ),
-              PropertyTemplate(
-                name: Strings.NEW,
-                value:
-                    '${massConverter(this._box.weights.currentNetExplosive)}',
-              )
-            ],
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 3,
+        children: [
+          PropertyGameTemplate(
+            name: Strings.WIDTH,
+            value: '${sizeConverter(this._box.dimensions.width)}',
           ),
-        ),
+          PropertyGameTemplate(
+            name: Strings.LENGTH,
+            value: '${sizeConverter(this._box.dimensions.length)}',
+          ),
+          PropertyGameTemplate(
+            name: Strings.HEIGHT,
+            value: '${sizeConverter(this._box.dimensions.height)}',
+          ),
+          PropertyWithHintTemplate(
+            hint: Strings.HINT_NET_WEIGHT,
+            propertyName: Strings.NET_WEIGHT,
+            propertyValue: '${massConverter(this._box.weights.net)}',
+          ),
+          PropertyWithHintTemplate(
+            hint: Strings.HINT_GROSS_WEIGHT,
+            propertyName: Strings.GROSS_WEIGHT,
+            propertyValue: '${massConverter(this._box.weights.currentGross)}',
+          ),
+          PropertyGameTemplate(
+            name: Strings.NEW,
+            value: '${massConverter(this._box.weights.currentNetExplosive)}',
+          )
+        ],
       ),
     );
   }
