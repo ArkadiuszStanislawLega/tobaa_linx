1206910379e9c6f231df4d0c636b24da5926a419
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-09 13:53:56 +0100

    Update transport detail view - properties in grid.

--- lib/widgets/Pages/DetailViews/transport_detail_view.dart
+++ lib/widgets/Pages/DetailViews/transport_detail_view.dart
@@ -5,6 +5,7 @@ import 'package:tobaa/database/db_cars.dart';
 import 'package:tobaa/main.dart';
 import 'package:tobaa/transport/transport.dart';
 import 'package:tobaa/widgets/ListViews/cars_list_view.dart';
+import 'package:tobaa/widgets/Templates/property_game_template.dart';
 import 'package:tobaa/widgets/Templates/property_template.dart';
 
 class TransportDetailView extends StatelessWidget {
@@ -29,27 +30,32 @@ class TransportDetailView extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            Column(
+            GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 3,
               children: [
-                PropertyTemplate(
+                PropertyGameTemplate(
                   name: Strings.NUMBER_OF_THE_BAA,
                   value: '${this._transport.capacity()}',
                 ),
-                PropertyTemplate(
+                PropertyGameTemplate(
                   name: Strings.NEW,
                   value:
                       '${massConverter(this._transport.transportNetExplosiveWeight)}',
                 ),
-                PropertyTemplate(
+                PropertyGameTemplate(
                   name: Strings.WEIGHT_OF_ALL_BAA,
                   value: '${massConverter(this._transport.transportNetWeight)}',
                 ),
-                PropertyTemplate(
+                PropertyGameTemplate(
                   name: Strings.GROSS_WEIGHT,
                   value:
                       '${massConverter(this._transport.transportGrossWeight)}',
                 ),
-                PropertyTemplate(
+                PropertyGameTemplate(
                   name: Strings.NUMBER_OF_CARS,
                   value: '${this._transport.numberOfCars}',
                 ),
