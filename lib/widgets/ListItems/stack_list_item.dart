8de8c0d9e8285aae3025fc2327cca4c1ee94425e
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-11-11 11:10:51 +0100

    Update stack list item - style update.

--- lib/widgets/ListItems/stack_list_item.dart
+++ lib/widgets/ListItems/stack_list_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:tobaa/Constants/strings.dart';
 import 'package:tobaa/database/db_assets.dart';
@@ -30,26 +31,58 @@ class StackListItem extends StatelessWidget {
       baa += "${ba.name}: $value szt., ";
     });
 
-    return Column(
-      children: [
-        Text(
-          '${Strings.STACK} ${this._index + 1}: ',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
+    return Container(
+      color: Colors.grey,
+      child: Row(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(7),
+            color: Colors.grey,
+            child: Column(
+              children: [
+                Text(
+                  'Stos:',
+                  style: TextStyle(fontSize: 11, color: Colors.black45),
+                ),
+                Text(
+                  '${this._index + 1}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
-        ),
-        Text(
-            '${Strings.NEW} ${massConverter(this._stack.weights.netExplosive)}'),
-        Text(
-            '${Strings.NET_WEIGHT} ${massConverter(this._stack.weights.net)}'),
-        Text(
-            '${Strings.GROSS_WEIGHT} ${massConverter(this._stack.weights.gross)}'),
-        Text(
-            '${Strings.NUMBER_OF_THE_CONTAINERS} ${this._stack.currentNumberOfBoxes}'),
-        Text(
-            '${Strings.NUMBER_OF_THE_BAA} ${this._stack.battleAirAssetCapacities.current}'),
-        Text('$baa')
-      ],
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 15.0),
+              color: Colors.white30,
+              child: Text('$baa', textAlign: TextAlign.center,),
+            ),
+          ),
+        ],
+      ),
     );
+
+    // return Column(
+    //   children: [
+    //     Text(
+    //       '${Strings.STACK} ${this._index + 1}: ',
+    //       style: TextStyle(
+    //         fontWeight: FontWeight.w600,
+    //       ),
+    //     ),
+    //     Text(
+    //         '${Strings.NEW} ${massConverter(this._stack.weights.netExplosive)}'),
+    //     Text(
+    //         '${Strings.NET_WEIGHT} ${massConverter(this._stack.weights.net)}'),
+    //     Text(
+    //         '${Strings.GROSS_WEIGHT} ${massConverter(this._stack.weights.gross)}'),
+    //     Text(
+    //         '${Strings.NUMBER_OF_THE_CONTAINERS} ${this._stack.currentNumberOfBoxes}'),
+    //     Text(
+    //         '${Strings.NUMBER_OF_THE_BAA} ${this._stack.battleAirAssetCapacities.current}'),
+    //     Text('$baa')
+    //   ],
+    // );
   }
 }
