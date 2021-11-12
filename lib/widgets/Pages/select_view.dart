7a569c1c67bfa47e5b7c003ddfb8b3e795bc1e55
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-11-12 19:15:53 +0100

    Update select view - make stateful widget.

--- lib/widgets/Pages/select_view.dart
+++ lib/widgets/Pages/select_view.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tobaa/Constants/url.dart';
+import 'package:tobaa/main.dart';
 import 'package:tobaa/widgets/Templates/main_menu.dart';
 
 import '../ListViews/available_baa_list_view.dart';
 
-class SelectView extends StatelessWidget {
+class SelectView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _SelectView();
+  }
+}
+
+class _SelectView extends State<SelectView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.cleaning_services),
+            onPressed: () => {
+              setState(
+                () {
+                  TOBAAApp.values.clear();
+                },
+              ),
+            },
+          ),
           IconButton(
             icon: Icon(Icons.double_arrow),
             onPressed: () => {
