2b1d3a25bc1db5cd23ed97cb4603a0b3899e7be3
Arkadiusz Stanisław Łęga <36114010+ArkadiuszStanislawLega@users.noreply.github.com>
2021-11-18 05:13:07 +0100

    Update baa list item - added type of asset in the background.

--- lib/widgets/ListItems/baa_list_item.dart
+++ lib/widgets/ListItems/baa_list_item.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:tobaa/constants/app_colors.dart';
-import 'package:tobaa/constants/app_strings.dart';
 import 'package:tobaa/battle_air_asset/battle_air_asset.dart';
+import 'package:tobaa/constants/app_strings.dart';
 import 'package:tobaa/database/db_boxes.dart';
 import 'package:tobaa/widgets/Templates/chip_template.dart';
 
@@ -37,67 +36,86 @@ class _BaaListItemState extends State<BaaListItem> {
             topRight: Radius.circular(20),
             bottomRight: Radius.circular(10)),
       ),
-      child: Column(children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 5, 5, 10),
-          decoration: BoxDecoration(
-            color: Colors.white30,
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10), topRight: Radius.circular(20)),
+      child: Column(children: [this._header(), this._footer()]),
+    );
+  }
+
+  Widget _header() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(10, 5, 5, 10),
+      decoration: BoxDecoration(
+        color: Colors.white30,
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10), topRight: Radius.circular(20)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ChipTemplate(
+            label: this.baa.name,
+            backgroundColor: Colors.lightBlue,
+            fontColor: Colors.white,
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
+          SizedBox(width: 5),
+          ChipTemplate(
+            label: '${DatabaseBoxes.container[this.baa.boxType]!.name}',
+            backgroundColor: Colors.amberAccent,
+            fontColor: Colors.black,
+          ),
+          SizedBox(width: 5),
+          ChipTemplate(
+            label: this.baa.explosionClass.toString(),
+            backgroundColor: Colors.deepOrange,
+            fontColor: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _footer() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
+      child: Stack(
+        children: [
+          Text(
+            'Bomby',
+            style: TextStyle(
+              color: Colors.black12,
+              fontFamily: 'MiddleAgesDeco',
+              fontSize: 35,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              ChipTemplate(
-                label: this.baa.name,
-                backgroundColor: Colors.lightBlue,
-                fontColor: Colors.white,
+              Text(
+                TOBAAApp.values.containsKey(baa.type)
+                    ? '${TOBAAApp.values[baa.type]!}'
+                    : '${0}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              SizedBox(width: 5),
-              ChipTemplate(
-                label: '${DatabaseBoxes.container[this.baa.boxType]!.name}',
-                backgroundColor: Colors.amberAccent,
-                fontColor: Colors.black,
-              ),
-              SizedBox(width: 5),
-              ChipTemplate(
-                label: this.baa.explosionClass.toString(),
-                backgroundColor: Colors.deepOrange,
-                fontColor: Colors.white,
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 0, 5, 5),
+                decoration: BoxDecoration(
+                  color: Colors.white30,
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(10),),
+                ),
+                child: ElevatedButton(
+                  child: Text(Strings.ADD),
+                  onPressed: this._addBaa,
+                ),
               ),
             ],
           ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(5, 0, 0, 5),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Text(
-              TOBAAApp.values.containsKey(baa.type)
-                  ? '${TOBAAApp.values[baa.type]!}'
-                  : '${0}',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.fromLTRB(10, 0, 5, 5),
-              decoration: BoxDecoration(
-                color: Colors.white30,
-                borderRadius:
-                    BorderRadius.only(bottomLeft: Radius.circular(10)),
-              ),
-              child: ElevatedButton(
-                child: Text(Strings.ADD),
-                onPressed: this._addBaa,
-              ),
-            ),
-          ]),
-        )
-      ]),
+        ],
+      ),
     );
   }
 
