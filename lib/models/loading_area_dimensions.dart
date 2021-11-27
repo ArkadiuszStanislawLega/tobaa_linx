a20102374b8f641f566fa9d419d2a408e0890192
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-11-27 08:58:06 +0100

    Update loading area dimensions - set coordinates code review.

--- lib/models/loading_area_dimensions.dart
+++ lib/models/loading_area_dimensions.dart
@@ -9,24 +9,71 @@ class LoadingAreaDimensions extends Dimensions {
 
   int get occupiedCapacity {
     var occupiedCapacity = 0;
-    this._occupiedDimensions.forEach((dimensions){
+    this._occupiedDimensions.forEach((dimensions) {
       occupiedCapacity += dimensions.capacity;
     });
 
     return occupiedCapacity;
   }
 
-  bool _isCapacityFit(int capacity){
-    return  this.capacity >= this.occupiedCapacity + capacity;
+  bool _isCapacityFit(int capacity) {
+    return this.capacity >= this.occupiedCapacity + capacity;
   }
 
   bool isWillBeFit(Dimensions dimensions) {
     var isCapacityFit = this._isCapacityFit(dimensions.capacity);
+    if (this._occupiedDimensions.isNotEmpty && isCapacityFit)
+      return this._isValidated(dimensions);
     return isCapacityFit;
+  }
+
+  bool _isValidated(Dimensions dimensions) {
+    var lastElement = this._occupiedDimensions.last;
+    var pointExtremeRightAtTheTop =
+        this._pointExtremeRightAtTheTop(lastElement);
+    var pointFurthestLeft = this._pointFurthestLeft(lastElement);
+
+    pointExtremeRightAtTheTop.x += 1 + dimensions.width;
+
+    if (pointExtremeRightAtTheTop.x <= this.width) {
+      pointExtremeRightAtTheTop.y += dimensions.height;
+      if (pointExtremeRightAtTheTop.y <= this.length)
+        return true;
+      else {
+        pointFurthestLeft.y += 1;
+        pointFurthestLeft.x += dimensions.width;
+        if (pointFurthestLeft.x <= this.width) {
+          pointFurthestLeft.y += dimensions.length;
+          if (pointFurthestLeft.y <= this.length) return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  Coordinates _pointFurthestLeft(Dimensions dimensions) {
+    Coordinates answer = Coordinates();
+    dimensions.coordinates.forEach((current) {
+      var isXLess = answer.x < 0 || current.x < answer.x;
+      var isYGrater = current.y > answer.y;
+      if (isXLess && isYGrater) answer = current;
+    });
+    return answer;
+  }
+
+  Coordinates _pointExtremeRightAtTheTop(Dimensions dimensions) {
+    Coordinates answer = Coordinates();
+    dimensions.coordinates.forEach((current) {
+      var isXGreater = current.x > answer.x;
+      var isYLess = answer.y < 0 || current.y < answer.y;
+      if (isXGreater && isYLess) answer = current;
+    });
+    return answer;
   }
 
   void append(Dimensions dimensions) {
     this._prepareDimensionsWhenIsFirst(dimensions);
+
     this._occupiedDimensions.add(dimensions);
     //
     // if (this.isWillBeFit(dimensions)) {
@@ -36,20 +83,31 @@ class LoadingAreaDimensions extends Dimensions {
     // }
   }
 
-  void _prepareDimensionsWhenIsFirst(Dimensions dimensions){
-    if (this._occupiedDimensions.isEmpty && this._isCapacityFit(dimensions.capacity)) {
-      this._setCoordinates(dimensions);
+  void _prepareDimensionsWhenIsFirst(Dimensions dimensions) {
+    if (this._occupiedDimensions.isEmpty &&
+        this._isCapacityFit(dimensions.capacity)) {
+      this._setCoordinates(dimensions, Coordinates(x: 0, y:0, z:0));
     }
   }
 
-  void _setCoordinates(Dimensions dimensions){
-    dimensions.coordinates.add(Coordinates(x:0, y:0, z:0));
-    dimensions.coordinates.add(Coordinates(x:dimensions.width, y:0, z:0));
-    dimensions.coordinates.add(Coordinates(x:0, y:dimensions.length, z:0));
-    dimensions.coordinates.add(Coordinates(x:dimensions.width, y:dimensions.length, z:0));
+  void _setCoordinates(Dimensions dimensions, Coordinates coordinates) {
+    dimensions.coordinates
+        .add(Coordinates(x: coordinates.x, y: coordinates.y, z: coordinates.z));
+    dimensions.coordinates.add(Coordinates(
+        x: coordinates.x + dimensions.width,
+        y: coordinates.y,
+        z: coordinates.z));
+    dimensions.coordinates.add(Coordinates(
+        x: coordinates.x,
+        y: coordinates.y + dimensions.length,
+        z: coordinates.z));
+    dimensions.coordinates.add(Coordinates(
+        x: coordinates.x + dimensions.width,
+        y: coordinates.y + dimensions.length,
+        z: coordinates.z));
   }
 
-  /*
+/*
   stary obszar x1
   nowy obszar x2
   1. biorę ostatni element z listy zajetych obszarów
@@ -71,7 +129,7 @@ class LoadingAreaDimensions extends Dimensions {
 
     b. nie - nie wystaje:
       - dodaje do nowego obszaru dlugosc obszaru, powiekszam os y
-      - sprawdzam czy nie wystaje z pojazdu
+      - sprawdzam czy wystaje z pojazdu
         - tak:
           - biore ostatni element z listy i jego punkty o najmniejszym x i najwiekszym y
           - powiekszam w osi y o dlugosc i sprawdzam czy sie miesci
