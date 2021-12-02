515fb4b5ea7eabb50ac26528e7dc7abb6607a568
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-12-02 18:50:56 +0100

    Update permissible_new_property_template.dart

---
+++ lib/widgets/Templates/permissible_new_property_template.dart
@@ -0,0 +1,16 @@
+part of widgets;
+
+class PermissibleNewPropertyTemplate extends StatelessWidget {
+  final double netExplosionWeightLimit;
+
+  PermissibleNewPropertyTemplate(this.netExplosionWeightLimit);
+
+  @override
+  Widget build(BuildContext context) {
+    return PropertyGameTemplate(
+      name: Strings.PERMISSIBLE_NEW,
+      value: '${massConverter(this.netExplosionWeightLimit)}',
+      background: Colors.grey[200]!,
+    );
+  }
+}
