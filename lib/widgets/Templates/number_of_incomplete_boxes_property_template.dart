4cf7734bd2284b3e271deabfdca4f05bc3af2cb5
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-12-02 18:50:40 +0100

    Update number_of_incomplete_boxes_property_template.dart

---
+++ lib/widgets/Templates/number_of_incomplete_boxes_property_template.dart
@@ -0,0 +1,16 @@
+part of widgets;
+
+class NumberOfIncompleteBoxesPropertyTemplate extends StatelessWidget {
+  final int numberOfIncompleteBoxes;
+
+  NumberOfIncompleteBoxesPropertyTemplate(this.numberOfIncompleteBoxes);
+
+  @override
+  Widget build(BuildContext context) {
+    return PropertyGameTemplate(
+      name: Strings.NUMBER_OF_INCOMPLETE_BOXES,
+      value: '$numberOfIncompleteBoxes',
+      background: Colors.grey[400]!,
+    );
+  }
+}
