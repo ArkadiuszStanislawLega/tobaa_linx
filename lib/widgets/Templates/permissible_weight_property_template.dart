4537015077cdba04bf7ac98526b16375f9961195
Arkadiusz Stanisław Łęga <arkadiusz.stanislaw.lega@gmail.com>
2021-12-02 18:51:04 +0100

    Update permissible_weight_property_template.dart

---
+++ lib/widgets/Templates/permissible_weight_property_template.dart
@@ -0,0 +1,16 @@
+part of widgets;
+
+class PermissibleWeightPropertyTemplate extends StatelessWidget {
+  final double weightLimit;
+
+  PermissibleWeightPropertyTemplate(this.weightLimit);
+
+  @override
+  Widget build(BuildContext context) {
+    return PropertyGameTemplate(
+      name: Strings.PERMISSIBLE_WEIGHT,
+      value: '${massConverter(this.weightLimit)}',
+      background: Colors.grey[200]!,
+    );
+  }
+}
